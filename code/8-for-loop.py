a1ff129718a61184152018a75198d6dcc2ab1adb
Javier Amaya <Javieramayapat@gmail.com>
2022-01-31 14:05:36 -0600

    For loop and Range

---
+++ code/8-for-loop.py
@@ -0,0 +1,20 @@
+def run():
+    """📑Create a loop that prints the number from 1 to 1000"""
+    # Example with while loop
+    # counter = 0
+    # while counter < 10:
+    #     counter += 1
+    #     print(counter)
+
+    """How to convert a range in a list
+    """
+    # range_example = range(1000)
+    # list_of_range = list(range_example)
+    # print(list_of_range)
+
+    for counter in range(1, 1001):
+        print(counter)
+
+
+if __name__ == "__main__":
+    run()
